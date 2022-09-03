a3cc866e587e533d46b51074d0147f9c72cb2c99
brssa120201 <brssa120201@gmail.com>
2022-09-02 19:07:33 -0500

    Crete models

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const express = require("express")
 const mongoose = require("mongoose")
+
+const app = express()
+const port = process.env.PORT||3000
+const uri=`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@electiva1.mtunbw2.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`
+
+mongoose.connect(uri)
+.then(console.log('data base connected'))
+.catch((error)=>console.log(error))
+app.listen(port, ()=>{console.log('Connect with the port', port)})
